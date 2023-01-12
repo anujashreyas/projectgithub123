e454e3a441df39fb31f97b824cebe5ce97cd79e7
nilesh <nilesh@LAPTOP-OMBPFNM9>
2023-01-12 20:52:29 +0530

    [anuja]: updated demo1 class

--- src/sample/demo1.java
+++ src/sample/demo1.java
@@ -1,5 +1,9 @@
 package sample;
 
-public class demo1 {
-
+public class demo1 
+{
+	public static void main(String[] args) 
+   	{	System.out.println("good evening");
+	    System.out.println("hello");
+    }
 }
